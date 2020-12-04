bebb1678cef8b582ab7c570252ada6164b56b90a
mkulagowski <mk.kulagowski@gmail.com>
2020-12-04 11:26:06 +0100

    4th day solution refactor
    Used reparse! for parsing key-value pairs
    and match for field validation.

--- src/days/day04.rs
+++ src/days/day04.rs
@@ -1,7 +1,28 @@
-use crate::common::Solution;
+use crate::{common::Solution, reparse};
 use lazy_static::lazy_static;
 use regex::Regex;
-use std::{collections::HashMap, fmt, str::FromStr, string::ParseError};
+use std::{collections::HashMap, str::FromStr, string::ParseError};
+
+struct Passport {
+    data: HashMap<String, String>,
+}
+
+impl FromStr for Passport {
+    type Err = ParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        lazy_static! {
+            static ref PASSPORT: Regex = Regex::new(r"(\w+):(.+)").unwrap();
+        }
+        let m: HashMap<String, String> = s
+            .split_whitespace()
+            .into_iter()
+            .map(|x| reparse!(x, PASSPORT, String, String).unwrap())
+            .collect();
+
+        Ok(Passport::new(m))
+    }
+}
 
 lazy_static! {
     static ref FIELDS: Vec<String> = vec![
@@ -12,40 +33,12 @@ lazy_static! {
         "hcl".to_string(),
         "ecl".to_string(),
         "pid".to_string(),
-        "cid".to_string(),
     ];
     static ref FOUR_DIG: Regex = Regex::new(r"^\d{4}$").unwrap();
     static ref NINE_DIG: Regex = Regex::new(r"^\d{9}$").unwrap();
     static ref HGT: Regex = Regex::new(r"^\d+(cm|in)$").unwrap();
     static ref HCL: Regex = Regex::new(r"^#[a-f0-9]{6}$").unwrap();
     static ref ECL: Regex = Regex::new(r"^(amb|b(lu|rn)|gr[yn]|hzl|oth)$").unwrap();
-}
-
-struct Passport {
-    data: HashMap<String, String>,
-}
-
-impl FromStr for Passport {
-    type Err = ParseError;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let m: HashMap<String, String> = s
-            .split_whitespace()
-            .into_iter()
-            .map(|x| {
-                let mut xx = x.split(':');
-                (xx.next().unwrap().to_owned(), xx.next().unwrap().to_owned())
-            })
-            .collect();
-
-        Ok(Passport::new(m))
-    }
-}
-
-impl fmt::Display for Passport {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        writeln!(f, "")
-    }
 }
 
 impl Passport {
@@ -75,50 +68,22 @@ impl Passport {
     }
 
     fn validate_fields(&self) -> bool {
-        let mut result = true;
+        let valid_fields = self
+            .data
+            .iter()
+            .filter(|&(key, val)| match key.as_str() {
+                "byr" => self.verify_four_dig(val, 1920, 2002),
+                "iyr" => self.verify_four_dig(val, 2010, 2020),
+                "eyr" => self.verify_four_dig(val, 2020, 2030),
+                "hgt" => self.verify_hgt(val),
+                "hcl" => HCL.is_match(val),
+                "ecl" => ECL.is_match(val),
+                "pid" => NINE_DIG.is_match(val),
+                _ => false,
+            })
+            .count();
 
-        if let Some(byr) = self.data.get(&"byr".to_string()) {
-            result &= self.verify_four_dig(byr, 1920, 2002);
-        } else {
-            return false;
-        }
-
-        if let Some(iyr) = self.data.get(&"iyr".to_string()) {
-            result &= self.verify_four_dig(iyr, 2010, 2020);
-        } else {
-            return false;
-        }
-
-        if let Some(eyr) = self.data.get(&"eyr".to_string()) {
-            result &= self.verify_four_dig(eyr, 2020, 2030);
-        } else {
-            return false;
-        }
-
-        if let Some(hgt) = self.data.get(&"hgt".to_string()) {
-            result &= self.verify_hgt(hgt);
-        } else {
-            return false;
-        }
-
-        if let Some(hcl) = self.data.get(&"hcl".to_string()) {
-            result &= HCL.is_match(hcl);
-        } else {
-            return false;
-        }
-        if let Some(ecl) = self.data.get(&"ecl".to_string()) {
-            result &= ECL.is_match(ecl);
-        } else {
-            return false;
-        }
-
-        if let Some(pid) = self.data.get(&"pid".to_string()) {
-            result &= NINE_DIG.is_match(pid);
-        } else {
-            return false;
-        }
-
-        result
+        valid_fields == FIELDS.len()
     }
 
     fn is_valid(&self) -> bool {
@@ -128,22 +93,23 @@ impl Passport {
             .map(|(k, _)| k)
             .filter(|k| FIELDS.contains(k))
             .count();
-        proper_fields == 8 || (proper_fields == 7 && !self.data.contains_key("cid"))
+        proper_fields == FIELDS.len()
     }
 }
 
 fn part1(input: &Vec<Passport>) -> String {
-    input.iter().filter(|x| x.is_valid()).count().to_string()
+    input.iter().filter(|&x| x.is_valid()).count().to_string()
 }
 
 fn part2(input: &Vec<Passport>) -> String {
     input
         .iter()
-        .filter(|x| x.validate_fields())
+        .filter(|&x| x.validate_fields())
         .count()
         .to_string()
 }
 
+// INPUT NEEDS TO BE PREFORMATTED -> 1 PASSPORT PER LINE!
 pub fn solve(raw_input: &[String]) -> Solution {
     let input: Vec<Passport> = raw_input
         .iter()
